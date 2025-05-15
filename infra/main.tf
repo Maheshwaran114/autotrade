dcb1e23921f9253807a254d0c40e0699614922d4
umamaheswaran <33101565+Maheshwaran114@users.noreply.github.com>
2025-05-15 18:25:48 +0530

    fix: make floating IP optional to avoid hitting account limits

--- infra/main.tf
+++ infra/main.tf
@@ -19,7 +19,10 @@ resource "digitalocean_droplet" "bn-trading" {
   ssh_keys = [var.ssh_key_id]
 }
 
+# Make floating IP optional using count
 resource "digitalocean_floating_ip" "bn-trading-ip" {
+  # Only create this resource if the create_floating_ip variable is true
+  count      = var.create_floating_ip ? 1 : 0
   droplet_id = digitalocean_droplet.bn-trading.id
   region     = digitalocean_droplet.bn-trading.region
 }
@@ -30,6 +33,7 @@ output "droplet_ip" {
 }
 
 output "floating_ip" {
-  value = digitalocean_floating_ip.bn-trading-ip.ip_address
-  description = "The floating IP address assigned to the Bank Nifty Trading server"
+  # Use the droplet's regular IP if no floating IP is created
+  value = var.create_floating_ip ? digitalocean_floating_ip.bn-trading-ip[0].ip_address : digitalocean_droplet.bn-trading.ipv4_address
+  description = "The IP address to use for the Bank Nifty Trading server (floating IP if available, otherwise regular IP)"
 }
